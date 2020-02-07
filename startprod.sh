488ba43d69d7a5eef1ae2b6fda7be743f7be8319
Ubuntu <ubuntu@ip-172-31-15-28.ec2.internal>
2020-02-07 16:33:24 +0000

    startup sh files

---
+++ startprod.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+pm2 delete all
+#exec fuser -n tcp -k 4200 && fuser -n tcp -k 4300
+
+while fuser -n tcp -k 4200; do
+    sleep 2
+done
+
+while fuser -n tcp -k 7000; do
+    sleep 2
+done
+
+
+echo "Starting User PWA -- PROD Portal Angular App ..................."
+pm2 start /home/ubuntu/work/user-pwa/pm2Files/prod.yaml --only userpwa
+sleep 2
+
+
+exec pm2 log
+
+
+# chmod a+x startup.sh
+
+# backend - 3000
+# AdminPortal - 4200
+# UserPortal - 5000
